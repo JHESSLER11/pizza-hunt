3a17644734fb911835d449f10f1381432a214147
JHessler <jhessler11@gmail.com>
2022-04-30 15:21:00 -0400

    comments

--- routes/api/index.js
+++ routes/api/index.js
@@ -4,6 +4,6 @@ const commentRoutes = require('./comment-routes')
 
 // add prefix of `/pizzas` to routes created in `pizza-routes.js`
 router.use('/pizzas', pizzaRoutes);
-router.use('/comment', commentRoutes);
+router.use('/comments', commentRoutes);
 
 module.exports = router;
